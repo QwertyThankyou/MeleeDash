e70045473381148f4d6574067dff9a7d1a4973ad
Revanh13 <vana130102@icloud.com>
2020-08-17 01:43:57 +0500

    Anime

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -3,53 +3,53 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class EnemyController : MonoBehaviour
 {
-    private int _health = 2;
-    private int _damage;
-    private float _speed = 1f;
+    private int _health = 2;     // Здоровье врага
+    private int _damage = 1;     // Урон
+    private float _speed = 1f;   // Скорость передвижения
 
-    private float _viewRad = 7.5f;
-    private float _patrolRad = 2.5f;
+    private float _viewRad = 7.5f;  // Радиус обнаружения
 
     private Transform _player;
-    private Transform _point;
-
     private Ball _ball;
+    
+    private float _speedRotate = 1f; // Скорость поворота
+    public int rotationOffset = -90; // Каким боком будет идти в сторону игрока
+    private float _rot;              // Хуй знает что такое
 
     void Start()
     {
         _player = GameObject.FindWithTag("Player").transform;
         _ball = GameObject.FindWithTag("Player").GetComponent<Ball>();
-        _point = transform;
     }
 
     void Update()
     {
-        //if (Vector2.Distance(transform.position, _point.position) < _patrolRad) Chill();
         if (Vector2.Distance(transform.position, _player.position) < _viewRad) Angry();
-        else GoBack();
     }
-
-    void Chill()
-    {
-    }
-
+    
     void Angry()
     {
         transform.position = Vector2.MoveTowards(transform.position, _player.position, _speed * Time.deltaTime);
+        
+        Vector2 difference = _player.position - transform.position;
+        _rot = Mathf.Atan2 (difference.y, difference.x) * Mathf.Rad2Deg;
+ 
+        Quaternion rotation = Quaternion.AngleAxis (_rot + rotationOffset, Vector3.forward);
+        transform.rotation = Quaternion.Slerp (transform.rotation, rotation, Time.deltaTime * _speedRotate);
     }
 
-    void GoBack()
-    {
-        transform.position = Vector2.MoveTowards(transform.position, _point.position, _speed * Time.deltaTime);
-
-    }
-
-    private void TakeDamage(int damage)
+    private void TakeDamage(int damage) // Наносит урон
     {
         _health -= damage;
+    }
+
+    private void GiveDamage(int damage) // Получает урон
+    {
+        _ball.health -= damage;
     }
 
     private void OnCollisionEnter2D(Collision2D other)
@@ -58,6 +58,15 @@ public class EnemyController : MonoBehaviour
         {
             TakeDamage(_ball.damage);
             if (_health <= 0) Destroy(this.gameObject);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            GiveDamage(_damage);
+            SceneManager.LoadScene(0);
         }
     }
 }
