d7fbfe0065f333aabb329e8062c97352b582434f
Revanh13 <vana130102@icloud.com>
2020-12-28 12:28:37 +0500

    Nice ss

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,10 +1,19 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using DefaultNamespace;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+    public Text money;
+    
+    private void Start()
+    {
+        money.text = Bank.money.ToString();
+    }
 
     public void PlayButton()
     {
